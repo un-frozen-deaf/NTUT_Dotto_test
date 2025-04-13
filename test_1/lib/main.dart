1070379dda5b7d79ea96172b6c9c639f5d71fb83
soraito <a231105@a.tsukuba-tech.ac.jp>
2025-04-13 16:24:55 +0900

    add 座標表示

--- test_1/lib/main.dart
+++ test_1/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -28,10 +26,22 @@ class _JpegMapScreenState extends State<JpegMapScreen> {
     'room204': Colors.grey,
   };
 
+  Offset? tapPosition;
+
   void _onRoomTapped(String roomId) {
     setState(() {
-      roomColors[roomId] = roomColors[roomId] == Colors.grey ? Colors.red : Colors.grey;
+      roomColors[roomId] =
+      roomColors[roomId] == Colors.grey ? Colors.red : Colors.grey;
     });
+  }
+
+  void _onImageTapped(TapDownDetails details) {
+    final RenderBox box = context.findRenderObject() as RenderBox;
+    final localPosition = box.globalToLocal(details.globalPosition);
+    setState(() {
+      tapPosition = localPosition;
+    });
+    debugPrint("Tapped at: ${localPosition.dx}, ${localPosition.dy}");
   }
 
   @override
@@ -39,45 +49,63 @@ class _JpegMapScreenState extends State<JpegMapScreen> {
     return Scaffold(
       appBar: AppBar(title: const Text('JPEGマップ')),
       body: Center(
-        child: Stack(
-          children: [
-            Image.asset(
-              'assets/map-2F.jpeg',
-              width: 400,
-              height: 600,
-              fit: BoxFit.contain,
-            ),
+        child: GestureDetector(
+          onTapDown: _onImageTapped,
+          child: Stack(
+            children: [
+              Image.asset(
+                'assets/map-2F.jpeg',
+                width: 400,
+                height: 600,
+                fit: BoxFit.contain,
+              ),
 
-            // 部屋1（例：上部）
-            Positioned(
-              left: 100,
-              top: 150,
-              child: GestureDetector(
-                onTap: () => _onRoomTapped('room203'),
-                child: Container(
-                  width: 60,
-                  height: 60,
-                  color: roomColors['room203']!.withOpacity(0.6),
-                  child: const Center(child: Text('203')),
+              // 座標表示
+              if (tapPosition != null)
+                Positioned(
+                  left: tapPosition!.dx,
+                  top: tapPosition!.dy,
+                  child: Container(
+                    color: Colors.black.withOpacity(0.7),
+                    padding: const EdgeInsets.all(4),
+                    child: Text(
+                      'x: ${tapPosition!.dx.toStringAsFixed(0)}, y: ${tapPosition!.dy.toStringAsFixed(0)}',
+                      style: const TextStyle(color: Colors.white, fontSize: 12),
+                    ),
+                  ),
+                ),
+
+              // 部屋1
+              Positioned(
+                left: 100,
+                top: 150,
+                child: GestureDetector(
+                  onTap: () => _onRoomTapped('room203'),
+                  child: Container(
+                    width: 60,
+                    height: 60,
+                    color: roomColors['room203']!.withOpacity(0.6),
+                    child: const Center(child: Text('203')),
+                  ),
                 ),
               ),
-            ),
 
-            // 部屋2（例：下部）
-            Positioned(
-              left: 200,
-              top: 300,
-              child: GestureDetector(
-                onTap: () => _onRoomTapped('room204'),
-                child: Container(
-                  width: 60,
-                  height: 60,
-                  color: roomColors['room204']!.withOpacity(0.6),
-                  child: const Center(child: Text('204')),
+              // 部屋2
+              Positioned(
+                left: 200,
+                top: 300,
+                child: GestureDetector(
+                  onTap: () => _onRoomTapped('room204'),
+                  child: Container(
+                    width: 60,
+                    height: 60,
+                    color: roomColors['room204']!.withOpacity(0.6),
+                    child: const Center(child: Text('204')),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
