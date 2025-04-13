03808098554f3f274c9e33562745a8cba2e412db
soraito <a231105@a.tsukuba-tech.ac.jp>
2025-04-13 16:13:37 +0900

    add room

--- test_1/lib/main.dart
+++ test_1/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,19 +10,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: SvgMapScreen(),
+      home: JpegMapScreen(),
     );
   }
 }
 
-class SvgMapScreen extends StatefulWidget {
-  const SvgMapScreen({super.key});
+class JpegMapScreen extends StatefulWidget {
+  const JpegMapScreen({super.key});
 
   @override
-  State<SvgMapScreen> createState() => _SvgMapScreenState();
+  State<JpegMapScreen> createState() => _JpegMapScreenState();
 }
 
-class _SvgMapScreenState extends State<SvgMapScreen> {
+class _JpegMapScreenState extends State<JpegMapScreen> {
   final Map<String, Color> roomColors = {
     'room203': Colors.grey,
     'room204': Colors.grey,
@@ -38,13 +37,47 @@ class _SvgMapScreenState extends State<SvgMapScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('校舎棟2Fマップ')),
+      appBar: AppBar(title: const Text('JPEGマップ')),
       body: Center(
-        child:Image.asset(
-          'assets/map-2F.jpeg',
-          width: 600,
-          height: 900,
-          fit: BoxFit.contain,
+        child: Stack(
+          children: [
+            Image.asset(
+              'assets/map-2F.jpeg',
+              width: 400,
+              height: 600,
+              fit: BoxFit.contain,
+            ),
+
+            // 部屋1（例：上部）
+            Positioned(
+              left: 100,
+              top: 150,
+              child: GestureDetector(
+                onTap: () => _onRoomTapped('room203'),
+                child: Container(
+                  width: 60,
+                  height: 60,
+                  color: roomColors['room203']!.withOpacity(0.6),
+                  child: const Center(child: Text('203')),
+                ),
+              ),
+            ),
+
+            // 部屋2（例：下部）
+            Positioned(
+              left: 200,
+              top: 300,
+              child: GestureDetector(
+                onTap: () => _onRoomTapped('room204'),
+                child: Container(
+                  width: 60,
+                  height: 60,
+                  color: roomColors['room204']!.withOpacity(0.6),
+                  child: const Center(child: Text('204')),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
